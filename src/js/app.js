3afa864366b8c4a07552833d4f6b06522724b587
ahmed_hameed <ahmed.phpdeveloper@gmail.com>
2020-06-12 02:17:22 +0500

    Cart Page Vue Integration & Mini Cart Static

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
 import Vue from "vue"
+window.Vue = Vue
+
+let axios = require('axios');
+window.axios = axios;
+
 let jQuery = require('jquery');
 window.jQuery = window.$ = jQuery;
+
 import "popper.js";
 import "bootstrap";
+
+window.Noty = require('noty');
+
+require('./components/ProductForm.js');
+require('./components/CartForm.js');
 require('./product.js'); 
+require('./header.js'); 
---
+++ src/js/components/ProductForm.js
@@ -0,0 +1,34 @@
+if (document.querySelector('.shopify-product-form')) {
+    let productForm = new Vue({
+        el:".shopify-product-form",
+        data(){
+            return{
+                form:{
+                    id: document.getElementById('variant_id').value ,
+                    quantity: 1,
+                }
+
+            }
+        },
+        methods:{
+            addToCart(){
+                axios.post('/cart/add.js', this.form )
+                .then(function (response) {                       
+                    new Noty({
+                        type: 'success',
+                        timeout: 3000,
+                        layout: 'topRight',
+                        text: 'Product added to cart!'
+                    }).show();
+                })
+                .catch(function (error) {
+                    new Noty({
+                        type: 'error',
+                        layout: 'topRight',
+                        text: 'Some notification text'
+                    }).show();
+                });
+            }
+        }
+    });   
+}
