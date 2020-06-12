91b102831cffaed86ea5dde1e1e86a9784a1a5de
ahmed_hameed <ahmed.phpdeveloper@gmail.com>
2020-06-12 18:05:50 +0500

    Mini Cart Stock setting

--- src/js/components/MiniCart.js
+++ src/js/components/MiniCart.js
@@ -1,97 +1,134 @@
-import {
-    store
-} from "../shared/cartData.js";
+import { store } from "../shared/cartData.js";
+import { cloneDeep } from "lodash/cloneDeep";
 
 if (document.querySelector('.mini-cart')) {
-    let productForm = new Vue({
-        el: ".mini-cart",
+    let MiniCart = new Vue({
+        el:".mini-cart",
         delimiters: ['${', '}'],
-
-        data() {
-            return {
+        data(){
+            return{
                 cartData: store.state.cartData,
             }
         },
 
         computed: {
-            cart() {
+            cart(){
                 return this.cartData[0];
             }
         },
 
-        created() {
+        created(){
             // mini cart is on every page, that's why, we cal it once here
             store.getCart();
         },
 
-        methods: {
+        methods:{
 
             // Remove item from cart
-            remove(item) {
+            remove(item){
                 let found = this.cart.items.find(product => product.variant_id == item.variant_id);
-                if (found) {
+                if(found){
 
-                    console.log("Found : ", found);
                     this.$delete(this.cart.items, this.cart.items.indexOf(found));
 
                 }
             },
 
             // Increment item by 1
-            increment(item) {
-
+            increment(item){
+                
                 let found = this.cart.items.find(product => product.variant_id == item.variant_id);
                 if (found) {
-                    item.quantity++;
+                    this.updateCart(found, 1);
                 }
 
 
             },
 
             // Decrement item by 1
-            decrement(item) {
+            decrement(item){
                 let found = this.cart.items.find(product => product.variant_id == item.variant_id);
                 if (found) {
-                    if (item.quantity < 2) {
-                        this.remove(item);
-                    } else {
-                        item.quantity--;
+
+                    if (item.quantity > 0) {
+                        this.updateCart(found, -1 );
                     }
                 }
 
             },
 
-            updateCart() {
+            updateCart(item, quantity){
 
-                let result = this.cart.items.reduce(
-                    (accumulator, target) => ({
-                        ...accumulator,
-                        [target.variant_id]: target.quantity
-                    }), {});
 
-                console.log(result);
+                let q = quantity + item.quantity;
 
-                axios.post('/cart/update.js', {
-                        updates: result
-                    })
-                    .then((response) => {
+                let data = {
+                    quantity: q,
+                    id: item.key
+                }
 
-                        new Noty({
-                            type: 'success',
-                            timeout: 3000,
-                            layout: 'topRight',
-                            text: 'Your cart items updated'
-                        }).show();
+                if (item.quantity < 1) {
+                    this.remove(item);
+                }
+
+                axios.post('/cart/change.js', data )
+                    .then( (response) => {
+
+                        // Find the current item and new item to compare the quanity
+                        let currentItem = this.cartData[0].items.find(product => product.variant_id == item.variant_id);
+                        let newItem = response.data.items.find(product => product.variant_id == item.variant_id);
+
+                        // If item exist
+                        if (currentItem) {
+                            // check if item quantity changed. Only change.js can detect this on response
+                            if(quantity > 0 && currentItem.quantity == newItem.quantity){
+
+                                new Noty({
+                                    type: 'warning',
+                                    timeout: 3000,
+                                    layout: 'topRight',
+                                    text: 'No more in stock'
+                                }).show();
+                                
+                            }else{
+                                // add one to current item
+                                currentItem.quantity += quantity;
+                                 new Noty({
+                                    type: 'success',
+                                    timeout: 3000,
+                                    layout: 'topRight',
+                                    text: 'Your cart items updated'
+                                }).show();
+
+                            }
+                        }
+                        
+                        
+                       
 
                     })
                     .catch(function (error) {
+
+                        console.log(error);
+
                         new Noty({
                             type: 'error',
                             layout: 'topRight',
                             text: 'There was something wrong!!'
                         }).show();
                     });
+               
+
             }
+
+         
+
         },
+
+       
     });
+
+
+
+    
 }
